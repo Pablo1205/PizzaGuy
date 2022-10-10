74fc02369d34682212349e76e9146c3a56aa2216
Robebou <guillaume.theret@efrei.net>
2022-10-10 21:38:40 +0200

    recup des pizza dispo en bdd au lancement et stockage dans l'appli

--- projet-S7-m1-application/Main.xaml.cs
+++ projet-S7-m1-application/Main.xaml.cs
@@ -1,4 +1,5 @@
-﻿using projet_S7_m1_application.Classes;
+﻿using MySql.Data.MySqlClient;
+using projet_S7_m1_application.Classes;
 using projet_S7_m1_application.Pages;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,37 @@ namespace projet_S7_m1_application
     {
         public Main()
         {
+            GetAvailablePizza();
+            GetAvailableDrink();
             InitializeComponent();
+        }
+
+        //database.conn
+
+        private void GetAvailablePizza()
+        {
+            Database newDatabse = new Database();
+            MySqlConnection conn = newDatabse.conn;
+            string sql = "SELECT * from Pizza";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            List<Pizza> listPizza = new List<Pizza>();
+            while (rdr.Read())
+            {
+                Pizza pizza = new Pizza((int)rdr[0],rdr[1].ToString(), rdr[2].ToString(), rdr[3].ToString());
+                listPizza.Add(pizza);
+            }
+            rdr.Close();
+            Application.Current.Properties["AvailablePizza"] = listPizza;
+            listPizza.ForEach(pizza =>
+            {
+                Console.WriteLine(pizza.GetName());
+            });
+            newDatabse.CloseConnection();
+        }
+        private void GetAvailableDrink()
+        {
+            Database newDatabse = new Database();
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
